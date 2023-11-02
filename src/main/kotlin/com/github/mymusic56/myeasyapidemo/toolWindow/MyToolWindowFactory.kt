6281c96170881dacdf9c93f9ace2188185fafd31
GitHub Action <action@github.com>
2023-11-02 15:02:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/mymusic56/myeasyapidemo/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/mymusic56/myeasyapidemo/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.mymusic56.myeasyapidemo.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.mymusic56.myeasyapidemo.MyBundle
+import com.github.mymusic56.myeasyapidemo.services.MyProjectService
 import javax.swing.JButton
 
 
